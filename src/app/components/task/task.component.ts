bc9ffc4061572ccd45624651548e3ce71792efac
AbnerJosue <josueestrada688@gmail.com>
2022-10-30 21:13:03 -0600

    feat: creating the component moduels and show the data in the services

--- src/app/components/task/task.component.ts
+++ src/app/components/task/task.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-
+import { TaskService } from '../../services/task.service'
 @Component({
   selector: 'app-task',
   templateUrl: './task.component.html',
   styleUrls: ['./task.component.css']
 })
-export class TaskComponent implements OnInit {
 
-  constructor() { }
+
+
+
+export class TaskComponent implements OnInit {
+  
+
+  tasks:any= [];
+
+  constructor(private tasksService: TaskService) { }
 
   ngOnInit(): void {
+    this.tasksService.getTask()
+    .subscribe(
+      res => {
+        console.log(res)
+        this.tasks = res.user; 
+      }, 
+      e => console.log(e)
+    )
   }
 
 }
--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from "@angular/common/http"
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
 
-  constructor() { }
+  private URL = 'http://localhost:4081'
+  
+
+  constructor(
+    private http: HttpClient
+  ) {
+    
+  }
+
+
+  getTask() {
+    return this.http.get<any>(`${this.URL}/gettingModuls`)
+  }
+
+
+  getPrivateTask() {
+    return this.http.get<any>(`${this.URL}/private-task`)
+  }
+
 }
